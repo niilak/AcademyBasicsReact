0e51bc78fd04b87cf647b1876165e518330f9631
niilak <nilakuzmina98@gmail.com>
2023-06-27 12:04:26 +0200

    make contactBanner modular

--- services-project/src/components/contactBanner/ContactBanner.jsx
+++ services-project/src/components/contactBanner/ContactBanner.jsx
@@ -1,11 +1,11 @@
 import Header02 from "../shared/title/Header02";
 import ContactButton from "../shared/contactBtn/ContactBtn";
 
-import "./contactBanner.css";
+import bannerStyles from "./contactBanner.module.css";
 
 function ContactBanner() {
   return (
-    <div className="contact-banner__container">
+    <div className={`${bannerStyles.contact_banner__container}`}>
       <Header02 text={"Like what you see?"} colorClass={"white"} />
       <p>Let's talk about it.</p>
       <ContactButton />
@@ -14,3 +14,8 @@ function ContactBanner() {
 }
 
 export default ContactBanner;
+
+const bannerStyles = {
+  contact_banner__container: "contact_banner__container_idddddddd",
+  surname: "njanja",
+};
